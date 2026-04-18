dabb7800a8b5aa79f1e6acdbab1d6f6c704af506
kringy12345-boop <kringy12345@gmail.com>
2026-04-18 17:46:39 +1000

    Set up Flask app with SQLite database and transaction form

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///finance.db"
+db = SQLAlchemy(app)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100))
+    amount = db.Column(db.Float)
+    type = db.Column(db.String(10))
+    
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "This is a finance tracker app!"
+
+@app.route("/add", methods =["POST"])
+def add_transaction():
+    description = request.form["description"]
+    amount = request.form["amount"]
+    type = request.form["type"]
+    transaction = Transaction(description=description, amount=amount, type=type)
+    db.session.add(transaction)
+    db.session.commit()
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
